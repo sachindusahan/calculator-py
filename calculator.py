2cbccaaea722c7f479da28ef4555337ae5f56585
sachindusahan <sachindusahan222@gmail.com>
2021-06-06 15:17:29 +0530

    1.2

--- calculator.py
+++ calculator.py
@@ -1,30 +1,4 @@
 from os import system
 
-i = 1 
-while i < 10:
-	calculator = input('Open Calculator > open  Exit > exit :')
-	if calculator == 'open':
-		try:
-			num_1 = float(input("FIRST Number >: "))
-			Op = input("Condition >: ")
-			num_2 = float(input("SECOND Number >: "))
-
-			def Condition(number_1,number_2):
-				if Op =='+':
-					print ((int(number_1)) + (int(number_2)))
-				elif Op == "-":
-					print ((int(number_1)) - (int(number_2)))
-				elif Op == "×":
-					print (int(number_1) * int(number_2))
-				elif Op == "÷":
-					print (int(number_1) / int(number_2))
-			Condition(num_1,num_2)
-		except ValueError as err:
-			print (err)
-	elif calculator == 'exit':
-		break
-		system('exit()')
-	else:
-		system('clear')
-
-	i +=1
+def fun():
+	print()

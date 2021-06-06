c7eabfaf081eafe95627e3e2020ff3de969f3406
Sachindu Sahan <>
2021-06-06 14:18:36 +0000

    updated

--- calculator.py
+++ calculator.py
@@ -1,4 +1,6 @@
-from os import system
-
-def fun():
-	x = 10
+def function(num1, operator, num2):
+    if operator == '+':
+        print(num1 + num2)
+    elif operator == '-':
+        print(num1 - num2)
+        

9550bc95e6a4ecaf150938a3e8be64005be3d3d2
sachindusahan <sachindusahan222@gmail.com>
2021-06-07 05:58:42 +0530

    1.4

--- calculator.py
+++ calculator.py
@@ -1,3 +1,21 @@
-def mine():
-    print()
-        
+def control(num1, operator, num2):
+    if operator == "+":
+        result = num1 + num2
+        print('>> ' + str(result))
+    elif operator == "*":
+        result = num1 * num2
+        print('>> ' + str(result))
+    elif operator == "-":
+        result = num1 - num2
+        print('>> ' + str(result))
+    elif operator == "/":
+        result = num1 / num2
+        print('>> ' + str(result))
+    else:
+        print("Invalid operator")
+
+number1 = int(input('First number: '))
+op = input("math operator: ")
+number2 = int(input("Second number: "))
+
+control(number1, op, number2)
